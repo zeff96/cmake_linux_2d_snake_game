5724a3c757ad2f147f78bd0818e99a33bfe97514
zeff96 <zeffo1975@gmail.com>
2024-06-19 18:51:11 +0300

    Add project set config

---
+++ out/build/CmakePresets.json/CMakeFiles/SnakeGame.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SnakeGame.dir/Sources/StateManager.cpp.o"
+  "CMakeFiles/SnakeGame.dir/Sources/StateManager.cpp.o.d"
+  "CMakeFiles/SnakeGame.dir/Sources/main.cpp.o"
+  "CMakeFiles/SnakeGame.dir/Sources/main.cpp.o.d"
+  "SnakeGame"
+  "SnakeGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SnakeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
