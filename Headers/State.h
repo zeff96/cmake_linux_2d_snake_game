9dcfd38f55d4b1dab481d32af0dd0f39ff862a31
zeff96 <zeffo1975@gmail.com>
2024-06-23 22:37:26 +0300

    Update state class header as abstract class

--- Headers/State.h
+++ Headers/State.h
@@ -7,15 +7,9 @@ namespace Engine
   class State
   {
   public:
-    State();
-    virtual ~State();
-
     virtual void Init() = 0;
     virtual void ProcessInput() = 0;
     virtual void Update(const sf::Time &deltaTime) = 0;
     virtual void Draw() = 0;
-
-    virtual void Pause() {};
-    virtual void Start() {};
   };
 }
